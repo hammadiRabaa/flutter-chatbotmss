1c841062cf3f62d11c5f6ac4edbbe0df556c97c6
Rabaa Hammadi <rabaa.hammadi@mssolutions-group.com>
2022-04-11 14:01:54 +0300

    fix

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ def response () :
     query = dict(request.form)['query']
     result = query
     return jsonify({"response" : result})
-    
+
+
+if __name__ != "__main__" : 
+    app.run(host="0.0.0.0",)
