a166d5d186b9b9c8542c52ae5c6e29b24d2c34d3
Rabaa Hammadi <rabaa.hammadi@mssolutions-group.com>
2022-04-12 10:53:03 +0300

    fixed

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify,request
 import time
 app = Flask(__name__);
-@app.route("https://flutter-chatbotappmss.herokuapp.com/bot", methods=["POST"])
+@app.route('/bot', methods=["POST"])
 def response():
     query = dict(request.form)['query']
     res = query + " " + time.ctime()
     return jsonify({"response" : res})
 if __name__=="__main__":
-    app.run(host="0.0.0.0",)
+    app.run(host="0.0.0.0",port='3000')
